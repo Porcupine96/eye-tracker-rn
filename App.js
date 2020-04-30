b361c0e6d929e82ecc6a69388a414a3aa7c3e8c9
Łukasz Kaźmierczak <luki.pol@gmail.com>
2020-04-30 21:30:50 +0200

    sync

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import Training from './training.js';
+import Track from './track.js';
 
 const App: () => React$Node = () => {
-  return <Training />;
+  // return <Training />;
+  return <Track />;
 };
 
 export default App;
